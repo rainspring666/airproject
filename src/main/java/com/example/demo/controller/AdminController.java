24f637482798e3da3d8881be9d76479ed73697e9
yuquanpeng <1979175539@qq.com>
2020-06-28 00:28:19 +0800

    调用师弟的接口完成分配操作

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -117,4 +118,5 @@ public class AdminController {
         return map;
 
     }
+
 }
