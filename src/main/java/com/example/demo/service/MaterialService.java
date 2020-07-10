e286b6f0941c38d4ff7be868612e924ed38aa5fa
lazy-coder12581 <1562670148@qq.com>
2020-07-10 17:03:12 +0800

    后台完成物料功能

--- src/main/java/com/example/demo/service/MaterialService.java
+++ src/main/java/com/example/demo/service/MaterialService.java
@@ -13,6 +13,33 @@ public class MaterialService {
 
     @Autowired
     MaterialMapper materialMapper;
+    public List<Material> get_material_by_params(String material_id, String ma_name){
+        if(material_id.equals("") && ma_name.equals(""))
+            return get_material_info();
+        List<Material> materialList = new LinkedList<>();
+        List<Material> materialList_id = new LinkedList<>(), materialList_name = new LinkedList<>();
+        if(!material_id.equals("")){
+            materialList_id = materialMapper.get_material_by_material_id(material_id);
+        }
+        if(!ma_name.equals("")){
+            materialList_name = materialMapper.get_material_by_ma_name(ma_name);
+        }
+        for (Material i: materialList_id) {
+            materialList.add(i);
+        }
+        for (Material i: materialList_name) {
+            boolean contain = false;
+            for (Material j: materialList_id) {
+                if(i.getMaterial_id().equals(j.getMaterial_id())){
+                    contain = true; break;
+                }
+            }
+            if(!contain)
+                materialList.add(i);
+        }
+        return materialList;
+    }
+
     public List<Material> get_material_info(){
 
         return materialMapper.get_material_info();
@@ -28,6 +55,12 @@ public class MaterialService {
 
     public boolean add_material_info(Material material){
 
+        // 检查是否编号重复
+        // 是否要保证物料名称独立？？？？？
+
+        Material old_material = get_one_info(material.getMaterial_id());
+        if(old_material != null)
+            return false;
         return materialMapper.add_material_info(material);
     }
     public boolean del_material_info(String material_id){
